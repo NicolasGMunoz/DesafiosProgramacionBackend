5cbe6a62b604467e52f11ebfb6f9780a201406ee
Nicolas G Munoz <nicolasgustavomunoz@gmail.com>
2024-01-04 18:23:23 -0300

    desafio3

---
+++ Desafio3/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import ProductManager from "./managers/productManager.js";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const productsPath = path.join(__dirname, './files/Products.json');
+
+const app = express();
+
+const productManager = new ProductManager(productsPath)
+
+app.get('/products', async (req, res) =>{
+    let products = await productManager.getProducts();
+    const limit = Number(req.query.limit);
+
+    if(limit){
+        let productsLimit = products.slice(0, limit);
+        return res.send(productsLimit);
+    }else{
+        res.send(products)
+    }
+
+});
+
+app.get('/products/:pid', async (req, res) => {
+    const product = await productManager.getProductById(Number(req.params.pid));
+    res.send(product)
+});
+
+
+app.listen(8080,()=>console.log("Listening on 8080"))
