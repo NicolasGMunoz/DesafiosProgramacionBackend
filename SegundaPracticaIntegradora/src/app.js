a831e70ed6a6bd7be492377373030bf2a4877711
Nicolas G Munoz <nicolasgustavomunoz@gmail.com>
2024-03-18 16:49:33 -0300

    finalizacion de entrega

--- SegundaPracticaIntegradora/src/app.js
+++ SegundaPracticaIntegradora/src/app.js
@@ -2,40 +2,39 @@ import express  from "express"
 import handlebars from "express-handlebars"
 import mongoose from "mongoose"
 import { __dirname } from "./util.js"
-import productsRouter from './routes/products.router.js'
-import cartsRouter from './routes/carts.router.js'
-import viewsRouter from './routes/views.router.js'
-import session from "express-session"
-import MongoStore from "connect-mongo"
-import sessionsRouter from './routes/sessions.router.js';
+import CoursesRouter from './routes/courses.routes.js'
+import StudentsRouter from './routes/students.routes.js'
+import UsersRouter from './routes/users.routes.js'
+import ViewsRouter from './routes/views.router.js'
+import { initializePassport } from "./config/passport.config.js";
 import passport from "passport"
 
 
 const app = express();
 
+const coursesRouter = new CoursesRouter()
+const studentsRouter = new StudentsRouter()
+const usersRouter = new UsersRouter()
+const viewsRouter = new ViewsRouter()
+
+initializePassport()
+app.use(passport.initialize());
+
+app.disable('x-powered-by')
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
-app.use(
-	session({
-		store: MongoStore.create({
-			client: mongoose.connection.getClient(),
-			ttl: 3600
-		}),
-		secret: "Coder55575secret",
-		resave: true,
-		saveUninitialized: true
-	})
-);
+
+
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'handlebars');
 
-app.use('/', viewsRouter);
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
-app.use("/api/sessions", sessionsRouter)
+app.use("/", viewsRouter.getRouter())
+app.use("/api/courses", coursesRouter.getRouter())
+app.use("/api/students", studentsRouter.getRouter())
+app.use("/api/users", usersRouter.getRouter())
 
 
 try {
